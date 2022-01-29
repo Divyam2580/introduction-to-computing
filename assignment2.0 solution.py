ce271b1933e8c11444f0838e4ac2961da9440d47
Divyam2580 <97753094+Divyam2580@users.noreply.github.com>
2022-01-29 22:27:27 +0530

    Add files via upload

---
+++ assignment2.0 solution.py
@@ -0,0 +1,40 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/divya/Documents/assignment2.0.py ==============
+QUESTION 1.)
+PART A
+35
+PART B
+egaugnal evitisnes esac a si nohtyP
+PART C
+SLICED STRING IS:  a case sensitive
+PART D
+Python is object oriented language
+PART E
+10
+PART F
+Pythonisacasesensitivelanguage
+QUESTION 2.)
+Hey,Divyam Sheokand Here!
+My SID is 21105099
+I am from ECE department and my CGPA is 10
+QUESTION 3.)
+a&b is:  8
+a|b is:  58
+a^b is:  50
+a<<2: 224 	b<<2:  40
+a>>2: 14 	b>>4:  0
+QUESTION 4.)
+ENTER THE FIRST NUMBER: 87
+ENTER THE SECOND NUMBER: 76
+ENTER THE THIRD NUMBER: 56
+THE GREATEST NUMBER IS 87
+QUESTION 5.)
+ENTER ANY STRING Divyam
+NO
+QUESTION 6.)
+ENTER LENGTH OF FIRST SIDE OF TRIANGLE: 34
+ENTER LENGTH OF SECOND SIDE OF TRIANGLE: 45
+ENTER LENGTH OF THIRD SIDE OF TRIANGLE: 78
+YES
