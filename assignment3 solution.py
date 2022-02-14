1e488948164ac3975248bb130d4e92b564ea847d
Divyam2580 <97753094+Divyam2580@users.noreply.github.com>
2022-02-15 00:16:24 +0530

    Add files via upload

---
+++ assignment3 solution.py
@@ -0,0 +1,94 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/divya/Documents/assignment3.py ===============
+THE SOLUTION OF QUESTION 1 :
+
+Enter the string: my name is divyam
+The occurence(s) of:
+		"my"    is/are   1 
+		"name"    is/are   1 
+		"is"    is/are   1 
+		"divyam"    is/are   1 
+
+THE SOLUTION OF QUESTION 2 :
+
+Day - 23
+Month - 10
+Year - 2002
+Next Date is: 24/10/2002
+
+THE SOLUTION OF QUESTION 3 :
+
+Enter the list: 2,3,4
+Output: [(2, 4), (3, 9), (4, 16)]
+
+THE SOLUTION OF QUESTION 4 :
+
+ENTER YOUR GRADE : 8.4
+YOUR GRADE IS B+ AND VERY GOOD PERFORMANCE
+
+THE SOLUTION OF QUESTION 5 IS :
+
+ABCDEFGHIJK
+ ABCDEFGHI 
+  ABCDEFG  
+   ABCDE   
+    ABC    
+     A     
+
+THE SOLUTION OF QUESTION 6 IS :
+
+ENTER THE NAME : Divyam Sheokand
+ENTER THE SID OF Divyam Sheokand: 21105099
+
+You have entered 1 value(s) 
+Do you want to enter more data? y
+ENTER THE NAME : saket
+ENTER THE SID OF saket: 21105105
+
+You have entered 2 value(s) 
+Do you want to enter more data? y
+ENTER THE NAME : chhavi
+ENTER THE SID OF chhavi: 21105076
+
+You have entered 3 value(s) 
+Do you want to enter more data? n
+PART A
+	THE SID OF 'Divyam Sheokand'   is   '21105099'
+	THE SID OF 'saket'   is   '21105105'
+	THE SID OF 'chhavi'   is   '21105076'
+PART B
+THE SID OF 'Divyam Sheokand'   is   '21105099'
+THE SID OF 'chhavi'   is   '21105076'
+THE SID OF 'saket'   is   '21105105'
+PART C
+THE SID OF 'chhavi'   is   '21105076'
+THE SID OF 'Divyam Sheokand'   is   '21105099'
+THE SID OF 'saket'   is   '21105105'
+PART D
+ENTER SID WHICH YOU WANT TO SEARCH : 21105099
+THE NAME OF THE STUDENT WHOSE SID IS '21105099' IS 'Divyam Sheokand' 
+
+THE SOLUTION OF QUESTION 7 IS :
+
+HOW MANY TERMS OF FIBONACCI SERIES DO YOU WANT : 7
+1 1 2 3 5 8 13 
+Average is : 4.71
+
+THE SOLUTION OF QUESTION 8 :
+
+PART A 
+{1, 3, 5, 6, 8}
+
+PART B 
+{17, 3, 6, 8, 9, 13}
+
+PART C 
+{1, 2, 4, 5}
+
+PART D 
+{6, 7, 8, 9, 10}
+
+PART E 
+{10, 7}
