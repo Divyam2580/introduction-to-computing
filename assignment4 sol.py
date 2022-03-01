0e64506dd0e4fc6ed01be7697e1171ae714da9f7
Divyam2580 <97753094+Divyam2580@users.noreply.github.com>
2022-03-02 00:46:08 +0530

    Add files via upload

---
+++ assignment4 sol.py
@@ -0,0 +1,92 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/divya/Documents/assignment4.py ===============
+QUESTION 1 :
+
+Move 1th disk from A to C
+Move 2th disk from A to B
+Move 1th disk from C to B
+Move 3th disk from A to C
+Move 1th disk from B to A
+Move 2th disk from B to C
+Move 1th disk from A to C
+
+QUESTION 2 :
+
+ENTER A NUMBER :12
+
+Using Loop
+
+           1           
+          1 1          
+         1 2 1         
+        1 3 3 1        
+       1 4 6 4 1       
+      1 5 10 10 5 1      
+     1 6 15 20 15 6 1     
+    1 7 21 35 35 21 7 1    
+   1 8 28 56 70 56 28 8 1   
+  1 9 36 84 126 126 84 36 9 1  
+ 1 10 45 120 210 252 210 120 45 10 1 
+1 11 55 165 330 462 462 330 165 55 11 1
+
+Using recursions:
+
+                      1   
+                    1   1   
+                  1   2   1   
+                1   3   3   1   
+              1   4   6   4   1   
+            1   5   10   10   5   1   
+          1   6   15   20   15   6   1   
+        1   7   21   35   35   21   7   1   
+      1   8   28   56   70   56   28   8   1   
+    1   9   36   84   126   126   84   36   9   1   
+  1   10   45   120   210   252   210   120   45   10   1   
+1   11   55   165   330   462   462   330   165   55   11   1   
+
+QUESTION 3
+
+ENTER TWO INTEGERS SEPERATED BY SPACE : 1 2
+(0, 1)
+Part A
+True
+
+Part B
+False
+
+Part C
+New Tuple : (0, 1, 4, 5, 6)
+Greater Than 4 : [5, 6]
+
+Part D
+{0, 1, 4, 5, 6}
+
+Part E
+FROZEN SET : frozenset({0, 1, 4, 5, 6})
+
+Part F
+6
+
+QUESTION 4 :
+Object Created!!
+The name of the student is Divyam and it's rollno is 21105099
+Object Destroyed!!
+
+QUESTION 5 :
+
+Part A
+The updated salary of Mehak is 70000
+
+Part B
+Viren record deleted!!
+
+QUESTION 6
+
+Enter word uttered by George: divyam
+Enter word made by Barbie: saket
+
+Do you want to consider same no. of letters also? no
+
+Barbie fails to form a word!  HENCE THEIR FRIENDSHIP IS FAKE.
